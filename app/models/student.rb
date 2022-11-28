be7b076a7c7e0b6afd9dd60250bbb07a44976b5e
Jose Aguilar <jaguilarny@gmail.com>
2022-11-27 20:07:03 -0500

    commit tree bde7253bb7b2e18afaf897d7da547d539364631a

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,9 @@
 class Student < ApplicationRecord
-
   def to_s
     "#{first_name} #{last_name}"
   end
 
+  def self.by_name(name)
+    where("first_name LIKE ? OR last_name LIKE ?", name, name)
+  end
 end
